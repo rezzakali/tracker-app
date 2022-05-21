f1e01e4f3f9625d6f28f39027b6f5a1606fc7e4c
Rezzak Ali <myrezzakalirk@gmail.com>
2022-05-21 19:44:35 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import Plot from "react-plotly.js";
+import "./App.css";
 
 function App() {
+  const [xValue, setXValue] = useState([]);
+  const [yValue, setYValue] = useState([]);
+
+  const apiCall = async () => {
+    let API_KEY = "81WVUHQDVUVSZDYO";
+    const symbol = "AMZN";
+    let myDataX = [];
+    let myDataY = [];
+
+    await fetch(
+      `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${symbol}&apikey=${API_KEY}`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        for (var key in data["Time Series (Daily)"]) {
+          myDataX.push(key);
+          myDataY.push(data["Time Series (Daily)"][key]["1. open"]);
+        }
+        setXValue(myDataX);
+        setYValue(myDataY);
+      })
+      .catch((error) => console.log(error.message));
+  };
+  useEffect(() => {
+    apiCall();
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Stock Tracker</h1>
+      <Plot
+        data={[
+          {
+            x: xValue,
+            y: yValue,
+            type: "scatter",
+            mode: "lines+markers",
+            marker: { color: "red" },
+          },
+          { x: xValue, y: yValue },
+        ]}
+        layout={{ width: 720, height: 440, title: "A Fancy Plot" }}
+      />
     </div>
   );
 }
